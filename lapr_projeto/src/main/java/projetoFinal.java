c0c454aa8429cc0d036382e6ae7b18415ffe01a4
João Marques <1192221@isep.ipp.pt>
2021-01-08 12:22:14 +0000

    Leitura através do teclado. Metodo getTotalPopulacao(); Metodo getValores();

--- lapr_projeto/src/main/java/projetoFinal.java
+++ lapr_projeto/src/main/java/projetoFinal.java
@@ -111,91 +111,55 @@ public class projetoFinal {
 
         lerFicheiro.close(); //Fecha o ficheiro
     }
+
     //======================================================================================================================
+    public static void introduzirDados(double[] matriz, double[] matrizSobrevivencia, double[] matrizNatalidade) {
+        String newFileName; //Variável que guarda o nome da espécie quando o utilizador insere os dados
+        System.out.println("Identifique a nova espécie a ser estudada :");
+        newFileName = sc.nextLine(); //Ler o nome da espécie
 
-    /*
-     * Verifica��o se o ficheiro obtido � v�lido ou n�o
-     */
-    private static boolean isFileValid(String result) {
-        return ((result != null) && (new File(result).isFile()));
-    }
+        System.out.println("Introduza as idades:");
+        int idades = sc.nextInt(); //Lê a quantidade de idades, ou faixas etárias
+        matriz = new double[idades];//Define a quantidade de faixas etárias
 
-    //Tentativa de cria��o do m�todo para criar um o gr�fico(incompleto)
-//    public static JavaPLot plotOneGraph(double auxArrPopulacao[], int auxLenght, boolean showOnScreen) throws IOException {
-//
-//
-//	JavaPlot p = new JavaPlot();
-//
-//	PlotStyle myPlotStyle = new PlotStyle();
-//        myPlotStyle.setStyle(Style.LINESPOINTS);
-//        myPlotStyle.setLineWidth(1);
-//        //RgbPlotColor = new RgbPlotColor(
-//        myPlotStyle.setLineType(NamedPlotColor.BLUE);
-//        myPlotStyle.setPointType(7);
-//        myPlotStyle.setPointSize(1);
-//
-//	double[] totalPopulacao = //Parametro String passado no getTotalPopulacao ...
-//	double matriz[][] = preencherGraphMatriz(auxArrPopulacao, auxLenght);
-//	//Falta a normalizada
-//
-//	p.getAxis("x").setLabel("", "Arial", 15);
-//        p.getAxis("y").setLabel("", "Arial", 15);
-//
-//        DataSetPlot s = new DataSetPlot(matriz);
-//        s.setTitle("Graph");
-//        s.setPlotStyle(myPlotStyle);
-//
-//        p.getAxis("x").setBoundaries(0, auxLength);
-//
-//        // p.newGraph();
-//        p.set("lmargin", "at screen 0.15");
-//        p.set("rmargin", "at screen 0.85");
-//        p.set("bmargin", "at screen 0.15");
-//        p.set("tmargin", "at screen 0.85");
-//        p.addPlot(s);
-//
-//        p.newGraph();
-//        if (showOnScreen) {
-//            p.plot();
-//        }
-//
-//        return p;
-//
-//    }
-
-    public static void savePNG(JavaPlot p, String name) {
-
-        FileTerminal png = new FileTerminal("png", name);
-        File file = new File(name);
-
-        try {
-            file.createNewFile();
-            png.processOutput(new FileInputStream(file));
-        } catch (FileNotFoundException ex) {
-            System.err.print(ex);
-        } catch (IOException ex) {
-            System.err.print(ex);
+        System.out.println("Distribuição inicial da população :");
+        for (int i = 0; i < idades; i++) {
+            System.out.println("Introduza valor:");
+            matriz[i] = sc.nextDouble(); //Lê a distribuição inicial da população, respetivamente.
         }
 
-        p.setTerminal(png);
-        p.plot();
-        /*
-         */
+        System.out.println("Taxa de Sobrevivência :");
+        matrizSobrevivencia = new double[idades];
+        for (int i = 0; i < idades; i++) {
+            System.out.println("Introduzir valor:");
+            matrizSobrevivencia[i] = sc.nextDouble(); //Lê a taxa de sobrevivência, respetivamente.
+        }
+
+        System.out.println("Taxa de Natalidade :");
+        matrizNatalidade = new double[idades];
+        for (int i = 0; i < idades; i++) {
+            System.out.println("Introduza valor :");
+            matrizNatalidade[i] = sc.nextDouble(); //Lê a taxa de natalidade, respetivamente.
+        }
+    }
+    //======================================================================================================================
+    public static double[] getTotalPopulacao(String[] linha) {
+        double[] matriz = new double[linha.length];
+        for (int i = 0; i < linha.length; i++) {
+            String[] objeto = linha[i].split("=");
+            matriz[i] = Double.parseDouble(objeto[i]);
+        }
+        return matriz;
     }
 
     //======================================================================================================================
-
-    public static double[][] multArrays(double[][]arr1, double[][]arr2){
-
-        double[][]arrM = new double[arr1.length][arr1.length];
-
-        for(int i = 0; i < arr1.length; i++){
-            for(int j = 0; j < arr1.length; j++){
-                for(int k = 0; k < arr1.length; k++){
-                    arrM[i][j] = arrM[i][j] + arr1[i][k] * arr2[k][j];
-                }
-            }
+    public static double[] getValores(String[] linha) {
+        double[] matriz = new double[linha.length];
+        for (int i = 0; i < linha.length; i++) {
+            String[] objeto = linha[i].trim().split("=");
+            matriz[i] = Double.parseDouble(objeto[i]);
         }
-        return arrM;
+        return matriz;
     }
+    //======================================================================================================================
 }
